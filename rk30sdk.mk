5abc53dd8521eab3124fe9401d09dcac038ae2d7
Bao Zhen <bz@infthink.com>
2014-10-26 11:13:52 +0800

    Enable configd, otad and logcat.
    Signed-off-by: Bao Zhen <bz@infthink.com>

--- rk30sdk.mk
+++ rk30sdk.mk
@@ -8,7 +8,12 @@ PRODUCT_NAME := rk30sdk
 PRODUCT_MODEL := rk30sdk
 PRODUCT_MANUFACTURER := rockchip
 
+PRODUCT_PACKAGES += \
+    configd \
+    otad \
+
 PRODUCT_PROPERTY_OVERRIDES += \
+			persist.service.logcat.enable = 1 \
 			ro.product.platform = MatchStick \
 			ro.product.version = 201401010000 \
 			ro.product.ota.host = www.rockchip.com:2300
